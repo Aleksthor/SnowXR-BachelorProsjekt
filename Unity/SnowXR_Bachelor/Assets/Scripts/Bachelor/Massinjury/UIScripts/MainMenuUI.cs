886bb97c33dd97e27d06956500dae640837f1524
Aleksthor <athorset98@gmail.com>
2024-04-15 19:58:55 +0200

    Fade Screen and some temporary audio sounds for UI and Game Complete

--- Unity/SnowXR_Bachelor/Assets/Scripts/Bachelor/Massinjury/UIScripts/MainMenuUI.cs
+++ Unity/SnowXR_Bachelor/Assets/Scripts/Bachelor/Massinjury/UIScripts/MainMenuUI.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using MassInjury.Utility;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +9,36 @@ namespace SnowXR.MassInjury
 {
     public class MainMenuUI : MonoBehaviour
     {
+        private int loadScene = 0;
+        private AudioSource audioSource;
+        [SerializeField] private FadeScreen fader;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
 
         public void StartGame()
         {
-            SceneManager.LoadScene(1);
+            audioSource.Play();
+            loadScene = 1;
+            fader.FadeOut();
         }
-        
+
+        public void FixedUpdate()
+        {
+            if (loadScene > 0)
+            {
+                switch (loadScene)
+                {
+                    case 1:
+                        if (audioSource.clip.length < audioSource.time + 0.1f)
+                            SceneManager.LoadScene(1);
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+        }
     }
 }
--- Unity/SnowXR_Bachelor/Assets/Scripts/Bachelor/Massinjury/UIScripts/SettingsUI.cs
+++ Unity/SnowXR_Bachelor/Assets/Scripts/Bachelor/Massinjury/UIScripts/SettingsUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using BNG;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MassInjury.Utility;
 
 namespace SnowXR.MassInjury
 {
@@ -11,6 +12,16 @@ namespace SnowXR.MassInjury
     {
         [SerializeField] private GameObject UI;
         [SerializeField] private ControllerBinding ToggleHandsInput = ControllerBinding.AButtonDown;
+        [SerializeField] private FadeScreen fader;
+
+        private AudioSource audioSource;
+
+        private int loadScene = 0;
+
+        private void Awake()
+        {
+            audioSource = GetComponentInChildren<AudioSource>();
+        }
 
         private void Start()
         {
@@ -24,16 +35,36 @@ namespace SnowXR.MassInjury
             {
                 UI.SetActive(!UI.activeSelf);
             }
+
+            if (loadScene > 0)
+            {
+                if (audioSource.clip.length < audioSource.time + 0.1f)
+                {
+                    switch (loadScene)
+                    {
+                        case 1:
+                            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                            break;
+                        case 2:
+                            SceneManager.LoadScene(0);
+                            break;
+                    }
+                }
+            }
         }
 
         public void RestartScene()
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            audioSource.Play();
+            loadScene = 1;
+            fader.FadeOut();
         }
         
         public void LoadMainMenu()
         {
-            SceneManager.LoadScene(0);
+            audioSource.Play();
+            loadScene = 2;
+            fader.FadeOut();
         }
     }
 }
